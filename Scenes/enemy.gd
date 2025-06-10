1169ef455a7797425c85c6a540ede1179b7675c1
Cinfa <cinfa78@gmail.com>
2025-06-10 22:27:57 +0200

    .

--- Scenes/enemy.gd
+++ Scenes/enemy.gd
@@ -1,5 +1,40 @@
+class_name Enemy
 extends CharacterBody2D
 
+@export var health = 10
+var dyingCoroutine = true
 
 func _physics_process(delta):
 	move_and_slide()
+
+func hurt(damage):
+	health-=damage
+	
+	$AudioStreamPlayer2D.play()
+	if health <= 0:
+		$Sprite2D.modulate = Color(1,0,0)
+		die()
+	else:		
+		hurtGraphics()
+func hurtGraphics():
+	$CollisionShape2D.disabled = true
+	$Sprite2D.modulate = Color(1,.5,0)
+	await get_tree().create_timer(0.2).timeout
+	$Sprite2D.modulate = Color(1,1,1)
+	$CollisionShape2D.disabled = false
+	
+func die():	
+	dyingCoroutine = true
+	$CollisionShape2D.queue_free()
+	while dyingCoroutine:
+		#await Engine.get_main_loop().process_frame
+		await RenderingServer.frame_post_draw
+		if !dyingCoroutine:			
+			queue_free()
+			break
+		
+
+
+func _on_audio_stream_player_2d_finished():
+	print("muori")
+	dyingCoroutine = false
--- Scenes/sword.gd
+++ Scenes/sword.gd
@@ -1,7 +1,7 @@
 extends RigidBody2D
 @export var angleIncrement = 10
 @export var angleAcceleration = 10
-
+@export var damage = 5
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
@@ -9,9 +9,14 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	rotation_degrees += angleIncrement
+	rotation_degrees = rotation_degrees + angleIncrement
 	angleIncrement += angleAcceleration
 
 
 func _on_self_destruct_timeout():
 	queue_free()
+
+func _on_area_2d_body_entered(body):
+	print("colpito")
+	if body is Enemy:
+		body.hurt(damage)
