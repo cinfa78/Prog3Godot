b1b15539b6a694aae2267e4217be7865fc00abe4
Cinfa <cinfa78@gmail.com>
2025-06-11 09:33:03 +0200

    flickering enemy

--- Scenes/enemy.gd
+++ Scenes/enemy.gd
@@ -18,8 +18,23 @@ func hurt(damage):
 		hurtGraphics()
 func hurtGraphics():
 	$CollisionShape2D.disabled = true
-	$Sprite2D.modulate = Color(1,.5,0)
-	await get_tree().create_timer(0.2).timeout
+	var timer = 0.2
+	var cycle = true
+	var time_now = Time.get_unix_time_from_system()
+	var time_start = Time.get_unix_time_from_system()
+	var time_elapsed = time_now - time_start
+	while cycle:
+		if !cycle:
+			break
+		$Sprite2D.modulate = Color(1,.5,0)
+		await Engine.get_main_loop().process_frame
+		$Sprite2D.modulate = Color(1,1,1)
+		await Engine.get_main_loop().process_frame
+		time_now = Time.get_unix_time_from_system()
+		time_elapsed = time_now - time_start
+		if time_elapsed>=timer:
+			cycle = false
+	#await get_tree().create_timer(0.2).timeout
 	$Sprite2D.modulate = Color(1,1,1)
 	$CollisionShape2D.disabled = false
 	
